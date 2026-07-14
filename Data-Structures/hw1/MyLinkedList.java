6e34651740af5be0274a2e3413939fffcb8276df
Amit Vaknin <96392747+amitvakk@users.noreply.github.com>
2026-07-14 22:55:40 +0300

    Update MyLinkedList.java

--- Data-Structures/hw1/MyLinkedList.java
+++ Data-Structures/hw1/MyLinkedList.java
@@ -7,9 +7,6 @@ public class MyLinkedList<T> {
         this.tail = null;
     }
     
-    /***
-     * Implement the following method.
-     */
     public void reverse() {
         ListLink<T> current = this.head;
         this.head = this.tail;
@@ -23,9 +20,6 @@ public class MyLinkedList<T> {
         }
     }
     
-    /***
-     * Assumes valid input (not null).
-     */
     public void insert(ListLink<T> element) {
     	element.setNext(head);
     	element.setPrev(null);    
@@ -36,9 +30,7 @@ public class MyLinkedList<T> {
     	head = element; 	
     }
     
-    /***
-     * Assumes valid input (pointer to link which is currentrently in the linked-list).
-     */
+    
     public void delete(ListLink<T> element) {
     	if (element == head && element == tail) {
             head = null;
