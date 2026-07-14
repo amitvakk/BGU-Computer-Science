6233ee713bf423c798bc6c8335d0f3bcb2b91801
Amit Vaknin <96392747+amitvakk@users.noreply.github.com>
2026-07-14 23:04:37 +0300

    Update ArrayElement.java

--- Data-Structures/hw3/ArrayElement.java
+++ Data-Structures/hw3/ArrayElement.java
@@ -1,17 +1,4 @@
-/**
- * This class represents a specific element in a specific data structure: 
- * an element in an array.
- * @param <T> The type of the satellite data.
- */
 public class ArrayElement<T> extends Element<T> {
-	/*
-     * You may add any fields that you wish to add.
-     * Remember that the use of built-in Java classes is not allowed,
-     * the only variables types you can use are: 
-     * 	-	the given classes in the assignment
-     * 	-	basic arrays
-     * 	-	primitive variables
-     */
 	private int index;		//The index of the element in the array.
 	
 	public ArrayElement(int key, T satelliteData) {
