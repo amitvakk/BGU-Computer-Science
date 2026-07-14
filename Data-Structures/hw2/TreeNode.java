1676a0d24c7f114489761c6dab4d8253c07f291a
Amit Vaknin <96392747+amitvakk@users.noreply.github.com>
2026-07-14 23:04:05 +0300

    Update TreeNode.java

--- Data-Structures/hw2/TreeNode.java
+++ Data-Structures/hw2/TreeNode.java
@@ -1,17 +1,4 @@
-/**
- * This class represents a specific element in a specific data structure: 
- * a node in an AVL-tree.
- * @param <T> The type of the satellite data.
- */
 public class TreeNode<T> extends Element<T> {
-	/*
-     * You may add any fields that you wish to add.
-     * Remember that the use of built-in Java classes is not allowed,
-     * the only variables types you can use are: 
-     * 	-	the given classes in the assignment
-     * 	-	basic arrays
-     * 	-	primitive variables
-     */
 	private TreeNode<T> left;
 	private TreeNode<T> right;
 	private TreeNode<T> p;		//parent
